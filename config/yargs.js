a5be92ce1b0dac9057ae7fa39cbf7b5d37c9f7f5
Irwin <irwinestradatorres@gmail.com>
2021-01-05 17:31:40 -0500

    Refactorizaciones Yargs Config

--- config/yargs.js
+++ config/yargs.js
@@ -1,29 +1,25 @@
+const descripcion = {
+    alias: 'd',
+    demand: true,
+    desc: 'Descripcion de la tarea por hacer'
+}
+
+const completado = {
+    alias: 'c',
+    default: true,
+    desc: 'Marca como completado o pendiente la tarea'
+}
+
 const argv = require('yargs')
     .command('crear', 'Crear un elemento por hacer', {
-        descripcion: {
-            alias: 'd',
-            demand: true,
-            desc: 'Descripcion de la tarea por hacer'
-        }
+        descripcion
     })
     .command('borrar', 'Borra un elemento por hacer', {
-        descripcion: {
-            alias: 'd',
-            demand: true,
-            desc: 'Descripcion de la tarea por hacer'
-        }
+        descripcion
     })
     .command('actualizar', 'Actualiza el estado completado de una tarea', {
-        descripcion: {
-            alias: 'd',
-            demand: true,
-            desc: 'Descripcion de la tarea por hacer'
-        },
-        completado: {
-            alias: 'c',
-            default: true,
-            desc: 'Marca como completado o pendiente la tarea'
-        }
+        descripcion,
+        completado
     })
     .help()
     .argv;
