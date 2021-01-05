0274a0f502c3cf678149230d7af528ed6049d8ab
Irwin <irwinestradatorres@gmail.com>
2021-01-05 16:28:14 -0500

    Crear Archivo Json

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // const argv = require('yargs').argv;
 const argv = require('./config/yargs').argv;
+const porHacer = require('./por-hacer/por-hacer');
 
 // console.log(argv);
 
@@ -7,7 +8,9 @@ let comando = argv._[0];
 
 switch (comando) {
     case 'crear':
-        console.log('Crear por hacer');
+        // console.log('Crear por hacer');
+        let tarea = porHacer.crear(argv.descripcion);
+        console.log(tarea);
         break;
     case 'listar':
         console.log('Mostrar todas las tareas por hacer');
---
+++ por-hacer/por-hacer.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+let listadoPorHacer = [];
+
+const guardarDB = () => {
+    let data = JSON.stringify(listadoPorHacer);
+
+    fs.writeFile(`db/data.json`, data, (err) => {
+        if (err)
+            throw new Error('No se pudo grabar');
+        else
+            console.log('El archivo ha sido creado');
+    });
+}
+
+const crear = (descripcion) => {
+    let porHacer = {
+        descripcion,
+        completado: false
+    };
+
+    listadoPorHacer.push(porHacer);
+    guardarDB();
+    return porHacer;
+}
+
+module.exports = {
+    crear
+}
